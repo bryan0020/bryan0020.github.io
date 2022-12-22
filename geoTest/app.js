5ce17f5576dcce49d7a2007cd92dedb5f4a32e13
bryan0020 <92699697+bryan0020@users.noreply.github.com>
2022-12-22 10:48:52 +0800

    Added new files

---
+++ geoTest/app.js
@@ -0,0 +1,49 @@
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  // getCurrentPosition(success, error, options) - options, long maximumAge, long timeout, boolean enableHighAccuracy
+  // maximumAge - device cache position, timeout, if enableHighAccuracy true - get best position, default false
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+function findMyPlace() {
+  if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition( (position) => {
+          console.log(position.coords);
+      }, (e) => {
+          console.log(e);
+      });
+  } else {
+      console.log("navigator not supported");
+  }
+}
+findMyPlace();
+
+// if user already set never, won't prompt
+// safari n chrome both works 4 iOS
---
+++ geoTest/app3.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    geoFindMe();
+    
+},
+
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    // getCurrentPosition(success, error, options) - options, long maximumAge, long timeout, boolean enableHighAccuracy
+    // maximumAge - device cache position, timeout, if enableHighAccuracy true - get best position, default false
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+})
