3c12091071a02568c663c76f68361464885f82fa
bryan0020 <92699697+bryan0020@users.noreply.github.com>
2022-12-22 16:43:29 +0800

    Added appNav4.js

---
+++ appNav4.js
@@ -0,0 +1,23 @@
+var x = document.getElementById("location");
+
+x.innerHTML = "";
+
+window.setInterval(
+    function () {
+        getLocation();
+    }, 
+    1000
+);
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
