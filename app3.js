73ebf8509593d0b8d47bbf9befe8e5a892aa3d62
bryan0020 <92699697+bryan0020@users.noreply.github.com>
2022-12-22 15:20:21 +0800

    Update app3.js
    ask location permission on page load

--- app3.js
+++ app3.js
@@ -1,39 +1,45 @@
+// ask permission when page load
 document.addEventListener("DOMContentLoaded", function() {
 
+    geoFindMe();
+
+})
+
+
+function geoFindMe() {
+
+    // to show status of user location acquiration
     const status = document.querySelector('#status');
     const mapLink = document.querySelector('#map-link');
   
     mapLink.href = '';
     mapLink.textContent = '';
   
+    // if successfully get location
     function success(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+    
+        status.textContent = '';
+        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
     }
   
     function error() {
-      status.textContent = 'Unable to retrieve your location';
+        status.textContent = 'Unable to retrieve your location';
     }
   
     // getCurrentPosition(success, error, options) - options, long maximumAge, long timeout, boolean enableHighAccuracy
     // maximumAge - device cache position, timeout, if enableHighAccuracy true - get best position, default false
     if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
+        Content = 'Geolocation is not supported by your browser';
     } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
+        status.textContent = 'Locating…';
+        navigator.geolocation.getCurrentPosition(success, error);
     }
-    
-})
 
-/*
-function geoFindMe() {
-
-    
+    const watchID = navigator.geolocation.watchPosition((position) => {
+        doSomething(position.coords.latitude, position.coords.longitude);
+    });
   
 }
-*/
