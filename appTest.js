b85bae7c239dbf41bbc624a09c3f3cec8926b47f
bryan0020 <92699697+bryan0020@users.noreply.github.com>
2022-12-27 16:50:39 +0800

    Update appTest.js

--- appTest.js
+++ appTest.js
@@ -4,9 +4,11 @@ setInterval(function () {
     watchId = navigator.geolocation.watchPosition(
         position => console.log(position)
     );
+    console.log("SetInterval: " + watchId);
 }, 5000)
 
 function buttonClickHandler() {
     // Cancel the updates when the user clicks a button.
     navigator.geolocation.clearWatch(watchId);
+    console.log("StopInterval: " + watchId);
 }
