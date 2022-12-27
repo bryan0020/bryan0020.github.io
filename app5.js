68aa2ab96af110a3245d7321feb1c52431bd4f33
bryan0020 <92699697+bryan0020@users.noreply.github.com>
2022-12-27 14:01:31 +0800

    Update app5.js

--- app5.js
+++ app5.js
@@ -1,50 +1,58 @@
+let timer;
+
+function startTimer() {
+    timer = setInterval(function() {
+        trackMe();
+    }, 5000);
+}
+ 
+function stopTimer() {
+    clearInterval(timer);
+}
+
 function trackMe() {
+    console.log("test");
 
     const status = document.querySelector('#status');
     const mapLink = document.querySelector('#map-link');
     const details = document.querySelector('#details');
 
-    mapLink.href = '';
-    mapLink.textContent = '';
-
     function success(position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         let time = timeConverter(position.timestamp);
-
-        status.textContent = '<p>Successfully locate location</p><br>';
-
+    
+        status.innerHTML = 'Location found successfully!!!';
+    
         mapLink.href = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=19/${latitude}/${longitude}`;
         mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-        
+    
         details.innerHTML =   'Latitude: '  + latitude + '<br />' +
                                 'Longitude: ' + longitude + '<br />' +
                                 'Time: ' + time + '<br />' +
                                 '<hr />' + details.innerHTML;
-        
     }
 
     function error() {
         status.textContent = 'Unable to retrieve your location';
     }
 
-    if (!navigator.geolocation) {
-        status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-        status.textContent = 'Locating...';
-        // navigator.geolocation.watchPosition(success, error, { timeout: 30000 });
-        // navigator.geolocation.getCurrentPosition(success, error);
-        navigator.geolocation.watchPosition(success, error);
-    }
     function timeConverter(UNIX_timestamp) {
         var d = new Date(UNIX_timestamp);
         console.log("d: " + d);
         return(d.toLocaleString());
     }
 
-    // Options: throw an error if no update is received every 30 seconds.
-    // var watchID = navigator.geolocation.watchPosition(success, error, { timeout: 30000 });
-
+    if (!navigator.geolocation) {
+        status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+        status.innerHTML = 'Locating...';
+        navigator.geolocation.getCurrentPosition(success, error, {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 30000 // large timeout to accomodate slow GPS lock on some devices
+        });
+    }
 }
 
-document.querySelector('#find-me').addEventListener('click', setInterval(trackMe(), 10000));
+document.querySelector('#find-me').addEventListener('click', startTimer);
