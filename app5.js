4a5b9261bcd72eefeaaa97349543ae3a00c9f7e1
bryan0020 <92699697+bryan0020@users.noreply.github.com>
2022-12-27 11:35:52 +0800

    Update app5.js

--- app5.js
+++ app5.js
@@ -2,10 +2,12 @@ function trackMe() {
 
     const status = document.querySelector('#status');
     const mapLink = document.querySelector('#map-link');
+    const details = document.querySelector('#details');
 
     status.textContent = '';
     mapLink.href = '';
     mapLink.textContent = '';
+    details.textContent = '';
 
     function success(position) {
         const latitude = position.coords.latitude;
@@ -15,10 +17,14 @@ function trackMe() {
         status.textContent = 'Successfully locate location';
 
         mapLink.href = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=19/${latitude}/${longitude}`;
-        mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} ° + '<br />' +
-        'Time: ' + time + '<br />' + 
-        '<hr />' + mapLink.textContent`;
-
+        mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} ° + <br /> +
+        'Time: ' + time + <br /> + 
+        <hr /> + mapLink.textContent`;
+        
+        details.textContent =   'Latitude: '  + latitude + '<br />' +
+                                'Longitude: ' + longitude + '<br />' +
+                                'Time: ' + time + '<br />' +
+                                '<hr />' + details.textContent;
     }
 
     function error() {
